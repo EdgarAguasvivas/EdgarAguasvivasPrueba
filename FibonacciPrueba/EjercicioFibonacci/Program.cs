1a6f0db7a18fdccec07f5eb561fced99116d7894
edagca_24 <edagca.24@gmail.com>
2021-01-27 22:43:16 -0400

    fibonacci

---
+++ FibonacciPrueba/EjercicioFibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EjercicioFibonacci
+{
+    class Program
+    {
+        static int FibonacciSeries(int lenght)
+        {
+            int first = 0, second = 1, result = 0;
+
+            if (lenght == 0) return 0;
+            if (lenght == 1) return 1; 
+
+            for (int i = 2; i <= lenght; i++)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce la cantidad: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", FibonacciSeries(i));
+            }
+            Console.ReadKey();
+        }
+
+    } 
+}
