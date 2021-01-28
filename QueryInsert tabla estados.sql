cc50110dfb55b7d7dea8b2e63316fdfca39aba46
edagca_24 <edagca.24@gmail.com>
2021-01-27 22:48:58 -0400

    scripts

---
+++ QueryInsert tabla estados.sql
@@ -0,0 +1,12 @@
+INSERT INTO DbEdgarAguasvivasPrueba.dbo.Estado(EstadoNombre)
+     VALUES('Abiertas')
+GO
+
+
+INSERT INTO DbEdgarAguasvivasPrueba.dbo.Estado(EstadoNombre)
+     VALUES('Aprobadas')
+	 
+GO
+
+INSERT INTO DbEdgarAguasvivasPrueba.dbo.Estado(EstadoNombre)
+VALUES('Canceladas')
